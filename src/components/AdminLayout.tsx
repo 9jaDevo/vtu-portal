3bc9eca80db8502e47d1292a0776eb1f16ac1874
danhackv <maocularsms@gmail.com>
2025-07-12 04:21:24 +0100

    Remove Redundant Left Padding

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -133,7 +133,7 @@ export function AdminLayout({ children }: AdminLayoutProps) {
       </div>
 
       {/* Main Content */}
-      <div className="lg:pl-72 lg:flex-1 w-full">
+      <div className="lg:flex-1 w-full">
         {/* Top Bar */}
         <div className="bg-white border-b border-gray-200 px-4 py-4 lg:px-8">
           <div className="flex items-center justify-between">
